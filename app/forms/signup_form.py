3f2588f58453b1c878f9bd59d0f6e465ed4eb4bf
Ricky Thang <rthang11@yahoo.com>
2022-01-23 21:39:48 -0800

    db - signup form - add password length validation

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -27,6 +27,14 @@ def username_within_char_range(form,field):
     if len(username) < 6:
         raise ValidationError('Username must contain at least six characters.')
 
+def password_within_char_range(form,field):
+    # Checking if password is between six to forty characters
+    password = field.data
+    if len(password) > 100:
+        raise ValidationError('Password must be 100 characters or fewer.')
+    if len(password) < 8:
+        raise ValidationError('Password must contain at least eight characters.')
+
 def name_is_not_too_long(form, field):
     # Checking if first name has fewer than fifty characters
     name = field.data
@@ -38,6 +46,6 @@ class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists, username_within_char_range])
     email = StringField('email', validators=[DataRequired(), Email(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired(), password_within_char_range])
     first_name = StringField('first_name', validators=[name_is_not_too_long, DataRequired()])
     last_name = StringField('last_name', validators=[name_is_not_too_long])
