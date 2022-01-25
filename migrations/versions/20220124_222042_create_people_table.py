10b88c0852f3af6179e001d44f991af3d89f11e6
Ricky Thang <rthang11@yahoo.com>
2022-01-24 22:43:27 -0800

    bug fix - migrations - add user_id type field and correct ARRAY typo

--- migrations/versions/20220124_222042_create_people_table.py
+++ migrations/versions/20220124_222042_create_people_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '76a083802815'
-down_revision = 'ffdc0a98111c'
+down_revision = 'aada66a51e2f'
 branch_labels = None
 depends_on = None
 
@@ -20,17 +20,17 @@ def upgrade():
     op.create_table('people',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.PrimaryKeyConstraint('id'),
-    sa.Column('username', sa.String(length=100), nullable=False),
+    sa.Column('name', sa.String(length=100), nullable=False),
     sa.Column('relationship', sa.String(60)),
     sa.Column('description', sa.Text),
-    sa.Column('websites', sa.Array(sa.String(255))),
-    sa.Column('social_media', sa.Array(sa.String(255))),
+    sa.Column('websites', sa.ARRAY(sa.String(255))),
+    sa.Column('social_media', sa.ARRAY(sa.String(255))),
     sa.Column('birthday', sa.Date),
-    sa.Column('email_addresses', sa.Array(sa.String(254))),
-    sa.Column('phone_numbers', sa.Array(sa.String(40))),
-    sa.Column('addresses', sa.Array(sa.Text)),
+    sa.Column('email_addresses', sa.ARRAY(sa.String(254))),
+    sa.Column('phone_numbers', sa.ARRAY(sa.String(40))),
+    sa.Column('addresses', sa.ARRAY(sa.Text)),
     sa.Column('company', sa.String(150)),
-    sa.Column('user_id', nullable=False),
+    sa.Column('user_id', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['user_id'], ['users.id']),
     sa.Column('created_at', sa.DateTime(), server_default=sa.sql.func.now()),
     sa.Column('updated_at', sa.DateTime(), server_default=sa.sql.func.now())
