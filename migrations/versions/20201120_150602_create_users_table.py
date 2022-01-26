ff8ff5dbfe2833e598dbb8d701ce94958fd359e6
Ricky Thang <rthang11@yahoo.com>
2022-01-25 19:24:58 -0800

    bug fix - migration - add created_at and updated_at columns to users table

--- migrations/versions/20201120_150602_create_users_table.py
+++ migrations/versions/20201120_150602_create_users_table.py
@@ -23,6 +23,8 @@ def upgrade():
     sa.Column('username', sa.String(length=40), nullable=False),
     sa.Column('email', sa.String(length=254), nullable=False),
     sa.Column('hashed_password', sa.String(length=255), nullable=False),
+    sa.Column('created_at', sa.DateTime(), server_default=sa.sql.func.now()),
+    sa.Column('updated_at', sa.DateTime(), server_default=sa.sql.func.now()),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
