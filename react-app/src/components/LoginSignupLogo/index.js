90dfcb4456dca858b45db06898ea35d8f636208f
Ricky Thang <rthang11@yahoo.com>
2022-01-25 17:13:34 -0800

    feature - login and signup forms - add NavLink to home from login and signup forms

--- react-app/src/components/LoginSignupLogo/index.js
+++ react-app/src/components/LoginSignupLogo/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const LoginSignupLogo = () => {
 
     return (
         <div className="MinimalFormFrame-prosopeople-logo">
-          <p>Insert Logo Here</p>
-          <h1>PROSOPEOPLE</h1>
+          <NavLink to="/" exact={true} style={{textDecoration: 'none', color: 'inherit'}}>
+            <p>Insert Logo Here</p>
+            <h1>PROSOPEOPLE</h1>
+          </NavLink>
           <p className='tagline'> Chronicle Your Relationships </p>
         </div>
     )
