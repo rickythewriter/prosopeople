f77f482acffcb89488be56d66d64a97bf7cca2fe
Ricky Thang <rthang11@yahoo.com>
2022-01-27 12:30:24 -0800

    feature - app - implement update feature for people

---
+++ app/api/person_routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, request, session
+from flask_login import login_required
+from app.models import db, Person
+from app.forms import PersonForm
+
+person_routes = Blueprint('people', __name__)
+
+@person_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def update_person(id):
+	person = Person.query.get(id)
+	form = PersonForm()
+	form['csrf_token'].data = request.cookies['csrf_token']
+	if form.validate_on_submit:
+		person.name = form.name.data
+		person.description = form.description.data
+		db.session.commit()
+		return person.to_dict()
+	return {'errors': validation_errors_to_error_messages(form.errors)}, 401
