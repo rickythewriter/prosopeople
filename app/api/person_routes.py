94801abe4d670ecb4d2eb770fa9c37b18a1fdd16
Ricky Thang <rthang11@yahoo.com>
2022-01-27 14:58:23 -0800

    feature - people - write delete route

--- app/api/person_routes.py
+++ app/api/person_routes.py
@@ -17,3 +17,13 @@ def update_person(id):
 		db.session.commit()
 		return person.to_dict()
 	return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+@person_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_person(id):
+	person = Person.query.get(id)
+	db.session.delete(person)
+	db.session.commit()
+	return {'message': 'Successfully Deleted Task'}
+	# Delete task from database
+	# return success message
