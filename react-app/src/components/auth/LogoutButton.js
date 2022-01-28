571685c14678131eff5692642d8389487e0f8226
Ricky Thang <rthang11@yahoo.com>
2022-01-27 18:39:33 -0800

    feature - logout - clear person state when logging out

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
+import { removePerson } from '../../store/person';
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
+    dispatch(removePerson());
   };
 
   return <button id='button-logout' onClick={onLogout}>Sign Out</button>;
