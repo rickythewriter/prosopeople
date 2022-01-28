48572dd4217c66a82491bcb3042f9a52d77735bb
Ricky Thang <rthang11@yahoo.com>
2022-01-28 11:25:35 -0800

    deployment - dependencies - regenerate requirements

---
+++ react-app/src/components/EntriesMenu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import EntryPreviewCard from './EntryPreviewCard'
+import './EntriesMenu.css'
+
+const EntriesMenu = () => {
+	// return (
+	// 	<div className="horizontal-panel-R horizontal-panel" id="container-entries">
+	// 		{/* map out a bunch of entry preview cards here */}
+	// 	</div>
+	// )
+}
