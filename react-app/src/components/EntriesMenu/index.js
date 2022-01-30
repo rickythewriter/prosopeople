5415be020d78ebffb7e89a7789a3cba5160d7637
Ricky Thang <rthang11@yahoo.com>
2022-01-29 23:48:28 -0800

    feature - entries menu - sort entries by updated_at

--- react-app/src/components/EntriesMenu/index.js
+++ react-app/src/components/EntriesMenu/index.js
@@ -1,18 +1,3 @@
-/*---------------------------------------------------------------------/
-	
-	Sort by most recently updated
-
-	Layout
-
-	<some card />
-	<Entries Preview Window>
-		<entry preview card1>
-		<entry preview card2>
-		...
-		<entry preview cardn>
-	</ Entries Preview Window>
-
-/---------------------------------------------------------------------*/
 import React, {useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { loadEntries } from '../../store/entries'
@@ -30,6 +15,19 @@ const EntriesMenu = ({setNewEntrySelected}) => {
 	useEffect(()=> {
 		dispatch(loadEntries(person))
 	}, [dispatch])
+
+	/* Sort entries array by date */
+	entries.sort(function(a,b) {
+		const timeA = new Date(a.updated_at) 
+		const timeB = new Date(b.updated_at)
+		if (timeA < timeB) {
+			return 1;
+		}
+		if (timeA > timeB) {
+			return -1
+		}
+		return 0
+	});
 
 	/* This component iterates each entry into an EntryPreviewCard */
 	return (
