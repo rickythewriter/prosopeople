aafd8f777ced437b7fe56b4c01c9eb3d761b9aac
Ricky Thang <rthang11@yahoo.com>
2022-01-29 23:34:57 -0800

    db - seeds - update seeds for somewhat lived-in effect

--- app/seeds/entries.py
+++ app/seeds/entries.py
@@ -8,18 +8,34 @@ def seed_entries():
     	user_id = 1,
         person_id = 1
     	)
-    entry2 = Entry( title = "What I remember about the night we went drinking",
+    entry2 = Entry( 
+        title = "What I remember about the night we went drinking",
         body = "", 
         user_id = 1,
         person_id = 2)
-    entry3 = Entry( title = "", 
+    entry3 = Entry( 
+        title = "", 
         body = "His enthusiasm for sleeping and for burritos was memorable.",
         user_id = 1,
         person_id = 3)
+    entry4 = Entry( 
+        title = "Everlong Cover",
+        body = "Hello, I\'ve waited here for you\nEverlong\nTonight, I throw myself into\nAnd out of the red\nOut of her head, she sang\n\nCome down, and waste away with me\nDown with me\nSlow, how you wanted it to be\nI\'m over my head\nOut of her head, she sang\n\nAnd I wonder\nWhen I sing along with you\n\nIf everything could ever feel this real forever\nIf anything could ever be this good again\nThe only thing I\'ll ever ask of you\nYou\'ve got to promise not to stop when I say when\nShe sang\n\nBreathe out, so I can breathe you in\nHold you in\nAnd now I know you\'ve always been\nOut of your head, out of my head I sang\n\nAnd I wonder\nWhen I sing along with you\n\nIf everything could ever feel this real forever\nIf anything could ever be this good again\nThe only thing I\'ll ever ask of you\nYou\'ve got to promise not to stop when I say when\nShe sang\n\nAnd I wonder\n\nIf everything could ever feel this real forever\nIf anything could ever be this good again\nThe only thing I\'ll ever ask of you\nYou\'ve got to promise not to stop when I say when",
+        user_id = 1,
+        person_id = 5
+        )
+    entry5 = Entry( 
+        title = "Walk Like a Panther",
+        body = "Marie has set up home\nWith a man who\'s half my age\nA halfwit in a leotard\nStands on my stage\n\nThe standards have fallen\nMy value has dropped\nBut don\'t shed a tear\nSome walk like they own the place\nWhilst others creep in fear\nTry if you can to walk like a man\nBut you don\'t come near\n\nYou\'ve got to fly like an eagle\nProwl like a lion in Africa\nLeap like a salmon\nHome from the sea\nTo keep up with me\nYou\'ve got to walk like a panther tonight\nWalk like a panther tonight\n\nThe old hometown just looks the same\nLike a derelict man who had died out of shame\nLike a jumble sale left out in the rain\nIt\'s not good, it\'s not right\n\nThe standards have fallen\nMy value has dropped\nBut don\'t shed a tear\nSome walk like they own the place\nWhilst others creep in fear\nTry if you can to walk like a man\nBut you, you don\'t come near\n\nYou\'ve got to fly like an eagle\nProwl like a lion in Africa\nLeap like a salmon\nHome from the sea\nTo keep up with me\nYou\'ve got to walk like a panther tonight\nWalk like a panther tonight\n\nWhere did you leave all self-respect?\nYou look like a reptile, your house is a wreck\nYour existence an insult\nStains that are suspect cover your clothes\n\nThe standards have fallen\nMy value has dropped\nBut don\'t shed a tear\nSome walk like they own the place\nWhilst others creep in fear\nTry if you can to walk like a man\nBut you, you don\'t come near\n\nYou\'ve got to fly like an eagle\nProwl like a lion in Africa\nLeap like a salmon\nHome from the sea\nTo keep up with me\nYou\'ve got to walk like a panther tonight\n\nWalk like a panther tonight\nWalk like a panther tonight\nWalk like a panther tonight\n\nYou\'ve got to walk like a panther\nWalk like a panther tonight\n\nTonight\nTonight\nWalk like a panther tonight",
+        user_id = 1,
+        person_id = 5
+        )
 
     db.session.add(entry1)
     db.session.add(entry2)
     db.session.add(entry3)
+    db.session.add(entry4)
+    db.session.add(entry5)
 
     db.session.commit()
 
