38d74899f458ca56800982de6ada16397149d57b
Ricky Thang <ricky.writer.dev@gmail.com>
2022-11-16 13:07:25 -0800

    feat - image model - write image model

---
+++ app/models/image.py
@@ -0,0 +1,33 @@
+from .db import db
+from sqlalchemy.sql import func
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    link = db.Column(db.String(1024))
+    caption = db.Column(db.Text)
+
+    #entry
+    entry_id = db.Column(db.Integer, db.ForeignKey("entries.id", ondelete="CASCADE"), nullable=False)
+    entry = db.relationship("Entry", back_populates="images")
+
+    #dossier
+    person_id = db.Column(db.Integer, db.ForeignKey("people.id", ondelete="CASCADE"), nullable=False)
+    person = db.relationship("Person", back_populates="images")
+
+    #user
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user = db.relationship("User", back_populates="images")
+
+    # recordkeeping
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, server_default=func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'link': self.link,
+            'caption': self.caption,
+        }
