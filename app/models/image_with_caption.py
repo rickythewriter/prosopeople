5589315aabfb09a3bc6ca1d6727cba75c12446b6
Ricky Thang <ricky.writer.dev@gmail.com>
2022-11-18 14:36:01 -0800

    refactor - flask models - rename image to image with caption

--- app/models/image_with_caption.py
+++ app/models/image_with_caption.py
@@ -1,7 +1,7 @@
 from .db import db
 from sqlalchemy.sql import func
 
-class Image(db.Model):
+class ImageWithCaption(db.Model):
     __tablename__ = 'images'
 
     id = db.Column(db.Integer, primary_key=True)
