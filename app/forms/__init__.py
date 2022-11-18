a965c40a1b2c2675c07c983b7d40ee61cd1c2bd5
Ricky Thang <ricky.writer.dev@gmail.com>
2022-11-18 14:37:46 -0800

    refactor - flask forms - rename image to image with captions

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -4,4 +4,4 @@ from .person_form import PersonForm
 from .entry_form import EntryForm
 from .tag_form import TagForm
 from .person_tag_form import PersonTagForm
-from .image_form import ImageForm
+from .image_with_caption_form import ImageWithCaptionForm
