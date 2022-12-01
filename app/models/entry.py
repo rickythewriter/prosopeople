f39a1f753d8a207420b47a9e109bdc328348ae5c
Ricky Thang <ricky.writer.dev@gmail.com>
2022-12-01 09:03:23 -0800

    fix - Entry model - image backpopulates entry

--- app/models/entry.py
+++ app/models/entry.py
@@ -18,7 +18,7 @@ class Entry(db.Model):
     person = db.relationship("Person", back_populates="entries")
 
     # images
-    images = db.relationship("ImageWithCaption", back_populates="entries", passive_deletes=True)
+    images = db.relationship("ImageWithCaption", back_populates="entry", passive_deletes=True)
 
     # recordkeeping
     created_at = db.Column(db.DateTime, server_default=func.now())
