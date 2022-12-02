f3dc49393bd73e96a61b841b9e2fe79984e9af39
Ricky Thang <ricky.writer.dev@gmail.com>
2022-12-02 14:10:01 -0800

    feat - image reel - create image reel component

--- react-app/src/components/Dashboard/MainView/ImageReel/ImageArea/index.js
+++ react-app/src/components/Dashboard/MainView/ImageReel/ImageArea/index.js
@@ -1,40 +1,46 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { uploadImage, createImage } from '../../../../../store/image'
 import './ImageArea.css'
 
-const ImageArea = ({isAddButton=false}) => {
+const ImageArea = ({image=null, isAddButton=false}) => {
 
     const user = useSelector(state => state.session.user);
     const person = useSelector(state => state.person);
     const entry = useSelector(state => state.entry);
-    const [caption, setCaption] = useState('');
-    const [errors, setErrors] = useState([])
 
     const dispatch = useDispatch()
 
     const changeHandler = async e => {
+
+        /* Upload image to AWS. Get file name. */
         const image = e.target.files[0]
         const jsonFilename = await dispatch(uploadImage(image));
         const filename = jsonFilename.filename;
 
+        /* Add image to database */
         const payload = {
             filename,
-            caption,
+            caption: '',
             entry_id: entry.id,
             person_id: person.id,
             user_id: user.id,
         }
-        console.log(payload);
-
-        const imageWithCaption = await dispatch(createImage(payload))
-        setErrors([])
+        dispatch(createImage(payload))
     }
 
-    if (isAddButton) {
+    if (image) {
         return (
-            <div className='image-area' id='upload-form'>
-                <label for='upload-image' id='upload-label'>
+            <div className='image-area'>
+                <img className='filmstrip-image' src={image.signed_url} alt={image.caption}/>
+            </div>
+        )
+    }
+
+    if (isAddButton){
+        return (
+            <div className='image-area upload-form'>
+                <label htmlFor='upload-image' id='upload-label'>
                     <input
                         type='file'
                         id='upload-image'
@@ -46,12 +52,13 @@ const ImageArea = ({isAddButton=false}) => {
                 </label>
             </div>
         )
-    }
+    }   
+
     return (
-        <div className='image-area' id='placeholder-image-area'>
-            {/* square */}
+        <div className='image-area placeholder-image-area'>
         </div>
     )
+    
 }
 
 export default ImageArea;
--- react-app/src/components/Dashboard/MainView/ImageReel/index.js
+++ react-app/src/components/Dashboard/MainView/ImageReel/index.js
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useSelector, useDispatch} from 'react-redux';
 import ImageArea from './ImageArea';
+import { loadImages } from '../../../../store/image';
 import './ImageReel.css'
+
+const ADD_BUTTON_PLACEHOLDER = {isAddButton: true};
 
 const ImageReel = () => {
 
-    // save image objects
-    // save range of image objects for display on filmstrip
-    // save functions for prev and next to be props for left and right arrows
+    const entry = useSelector(state => state.entry);
+    const images = useSelector(state => state.images);
+    const imageList = Object.values(images).sort((a,b) => new Date(b.created_at) - new Date(a.created_at))
+    const [reelStartIdx, setReelStartIdx] = useState(0);    
+
+    const dispatch = useDispatch();
+
+    /* When selected entry updates... */
+    useEffect(() => {
+        dispatch(loadImages(entry));
+        setReelStartIdx(0);
+    }, [entry])
+
+    const addButton = (<ImageArea isAddButton={true} />)
+    const filledImageAreas = imageList.map((image) => (<ImageArea image={image} />))
+    const imageAreaList = [addButton, ...filledImageAreas]
+
+    function getVisibleImageAreas(reelStartIdx, numImageAreas=3) {
+        const dummyImageArea = (<ImageArea />)
+        const visibleFilmStrip = imageAreaList.slice(reelStartIdx, reelStartIdx + numImageAreas)
+        while (visibleFilmStrip.length < numImageAreas) {
+            visibleFilmStrip.push(dummyImageArea);
+        }
+        return visibleFilmStrip;
+    }
+
+    function slideReel(forward, numImageAreas = 1) {
+        if (forward) {
+            const idxNext = reelStartIdx + numImageAreas;
+            const idxEndOfReel = imageList.length;
+            if (idxNext <= idxEndOfReel) setReelStartIdx(idxNext);
+        } else {
+            const idxPrev = reelStartIdx - numImageAreas;
+            if (idxPrev >= 0) setReelStartIdx(idxPrev);
+        }
+    }
 
     return (
-        <div id='reel'>
-            <div className='nav-arrow' id='left-arrow'>◀</div>
-            <div id="filmstrip">
-                <ImageArea isAddButton={true}/>
-                <ImageArea />
-                <ImageArea />
+        <>
+            <div id='reel'>
+                <div 
+                    className='nav-arrow' 
+                    id='left-arrow'
+                    onClick={() => slideReel(0)}
+                >
+                    ◀
+                </div>
+                <div id="filmstrip">
+                    {getVisibleImageAreas(reelStartIdx)}
+                </div>
+                <div 
+                    className='nav-arrow' 
+                    id='right-arrow'
+                    onClick={() => slideReel(1)}
+                >
+                    ▶
+                </div>
             </div>
-            <div className='nav-arrow' id='right-arrow'>▶</div>
-        </div>
+        </>
     )
 }
 
