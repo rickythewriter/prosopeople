b5f67be68befd11fb47fd8f9a3a99408b4eb93ca
Ricky Thang <ricky.writer.dev@gmail.com>
2022-12-02 15:41:32 -0800

    feat - flask api - write route for deleting images

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request, session
 from flask_login import login_required
 from app.models import db, ImageWithCaption
 from app.forms import ImageWithCaptionForm
-from app.s3_resources import upload_image_to_s3_bucket
+from app.s3_resources import upload_image_to_s3_bucket, delete_file_from_s3_bucket
 from datetime import datetime
 from sqlalchemy.sql import func
 from app.api.auth_routes import validation_errors_to_error_messages
@@ -35,29 +35,14 @@ def create_image_with_caption():
         return image_with_caption.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
-@image_routes.route('/<int_id>', methods=['PUT'])
-@login_required
-def update_image_caption(id):
-    image_with_caption = ImageWithCaption.query.get(id)
-    form = ImageWithCaptionForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        image_with_caption = ImageWithCaption()
-        image_with_caption.caption = form.caption.data
-        image_with_caption.updated_at = func.now()
-        db.session.commit()
-        return image_with_caption.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
-
-@image_routes.route('/<int_id>', methods=['DELETE'])
+@image_routes.route('/<int:id>', methods=['DELETE'])
 @login_required
 # Delete an image-with-caption object from database and from AWS
 def delete_image_with_caption(id):
 
     image_with_caption = ImageWithCaption.query.get(id)
 
-    s3_bucket = get_bucket()
-    delete_file_from_s3_bucket(image_with_caption.filename, s3_bucket)
+    delete_file_from_s3_bucket(image_with_caption.filename)
 
     db.session.delete(image_with_caption)
     db.session.commit()
