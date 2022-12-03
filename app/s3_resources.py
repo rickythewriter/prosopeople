ed42d0922efa8404becc847166088017a9547a50
Ricky Thang <ricky.writer.dev@gmail.com>
2022-12-03 10:18:16 -0800

    feat - s3 resources - add access and secret access to s3 resource

--- app/s3_resources.py
+++ app/s3_resources.py
@@ -2,14 +2,14 @@ import boto3
 from app.config import S3_BUCKET, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY
 
 def _get_s3_resource():
-    # if S3_ACCESS_KEY_ID and S3_SECRET_ACCESS_KEY:
-        # return boto3.resource(
-            # 's3', 
-            # aws_access_key_id = S3_ACCESS_KEY_ID, 
-            # aws_seret_access_key = S3_SECRET_ACCESS_KEY
-        # )
-    # else:
-    return boto3.resource('s3')
+    if S3_ACCESS_KEY_ID and S3_SECRET_ACCESS_KEY:
+        return boto3.resource(
+            's3', 
+            aws_access_key_id = S3_ACCESS_KEY_ID, 
+            aws_seret_access_key = S3_SECRET_ACCESS_KEY
+        )
+    else:
+        return boto3.resource('s3')
 
 def get_bucket():
     s3_resource = _get_s3_resource()
