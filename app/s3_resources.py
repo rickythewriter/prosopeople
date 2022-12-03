222b0d06ee20556a1168c378d54488ebad433b16
Ricky Thang <ricky.writer.dev@gmail.com>
2022-12-03 14:26:57 -0800

    fix - s3 resources - remove superfluous code from get_s3_resource

--- app/s3_resources.py
+++ app/s3_resources.py
@@ -2,14 +2,7 @@ import boto3
 from app.config import S3_BUCKET, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY
 
 def _get_s3_resource():
-    if S3_ACCESS_KEY_ID and S3_SECRET_ACCESS_KEY:
-        return boto3.resource(
-            's3', 
-            aws_access_key_id = S3_ACCESS_KEY_ID, 
-            aws_seret_access_key = S3_SECRET_ACCESS_KEY
-        )
-    else:
-        return boto3.resource('s3')
+    return boto3.resource('s3')
 
 def get_bucket():
     s3_resource = _get_s3_resource()
@@ -17,7 +10,7 @@ def get_bucket():
 
 def upload_image_to_s3_bucket(image):
     s3_bucket = get_bucket()
-    s3_bucket.Object(image.filename).put(Body=image)
+    s3_bucket.Object(image.filename).put(Body=image);
 
 def delete_file_from_s3_bucket(filename):
     s3_bucket = get_bucket()
