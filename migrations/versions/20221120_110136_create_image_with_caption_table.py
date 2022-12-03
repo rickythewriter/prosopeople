25737ebcf66b29f5765d876fab6ff07fd5cb485e
Ricky Thang <ricky.writer.dev@gmail.com>
2022-12-03 08:48:40 -0800

    build - migration - add render functions to latest revision

--- migrations/versions/20221120_110136_create_image_with_caption_table.py
+++ migrations/versions/20221120_110136_create_image_with_caption_table.py
@@ -7,7 +7,9 @@ Create Date: 2022-11-20 11:01:36.721685
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '54a6f44b97b3'
@@ -38,6 +40,8 @@ def upgrade():
     sa.Column('updated_at', sa.DateTime(), server_default=sa.sql.func.now()),
     )
 
+    if environment == "production":
+        op.execute(f"ALTER TABLE people_tags SET SCHEMA {SCHEMA};")
 
 def downgrade():
     op.drop_table('images')
